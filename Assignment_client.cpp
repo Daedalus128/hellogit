c0c788c5d055c1ff08cb323fa81aac8ab56ada14
Daedalus128 <Daedalus128@users.noreplu.github.com>
2018-02-05 18:48:44 -0400

    Updated client file

---
+++ Assignment_client.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Assignment.h"
+using namespace std;
+
+int main ()
+{
+int classNum = 0;
+
+string classWord = " ";
+vector<Assignment> testvector;
+
+Assignment object;
+
+testvector.push_back (object);
+
+for ( int x = 0; x < 4; x++)
+{
+cout << "enter an integer" << endl;
+cin >> classNum;
+cout << endl;
+cout << "enter any form of text" << endl;
+cin >> classWord;
+cout << endl;
+object.setNum (classNum);
+object.setWord (classWord);
+testvector.push_back (object.getNum, object.getWord);
+}
+cout << "The vector contains " << testvector.size() << " objects" << endl;
+cout << " The value of the objects in the vector are: " << endl;
+
+for (int y = 0; int y < testvector.size(); y++)
+{
+cout << testvector[y] << endl;
+}
+
+
+}
+
